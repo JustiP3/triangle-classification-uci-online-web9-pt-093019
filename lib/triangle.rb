a0f4b18a59b4fb52cf2c47bcdd6b4e2e93a63eb3
Justin Phillips <justinphil02@yahoo.com>
2019-11-10 23:10:37 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,6 +1,12 @@
 class Triangle
 attr_accessor :side1, :side2, :side3
 
+def initialize(x,y,z)
+  @side1 = x 
+  @side2 = y 
+  @side3 = z 
+end 
+
   class TriangleError < StandardError
     # triangle error code
   end
