6285d8db87cc4dae9da3cdf4006b745f4bf37bf3
Justin Phillips <justinphil02@yahoo.com>
2019-11-10 23:40:51 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,44 +7,44 @@ def initialize(x,y,z)
 end
 
 
-  class TriangleError < StandardError
-    def message
-      "All three sides must be greater than 0 and one side must be greater than the sum of the other two sides"
+class TriangleError < StandardError
+  def message
+    "All three sides must be greater than 0 and one side must be greater than the sum of the other two sides"
+  end
+end
+
+def kind
+#  binding.pry
+if self.valid?
+  self.determine
+end
+end #end of kind method
+  
+
+def valid?  #triangle inequality test and all sides > 0
+  self.sides.sort
+
+  if self.sides.detect {|side|  side <= 0 } || self.sides[2] < self.sides[1] + sef.sides[0]
+    begin
+      raise TriangleError
+      rescue TriangleError => error
+      puts error.message
     end
-  end
+  else
+    true
+  end #end of if block
 
-  def kind
+end #end of method valid?
 
-  #  binding.pry
-  if self.valid?
-    self.determine
-  end
-
-  end #end of kind method
-
-  def valid?  #triangle inequality test and all sides > 0
-    self.sides.sort
-
-    if self.sides.detect {|side|  side <= 0 } || self.sides[2] < self.sides[1] + sef.sides[0]
-      begin
-        raise TriangleError
-        rescue TriangleError => error
-        puts error.message
-      end
-    else
-      true
-    end #end of if block
-
-  end #end of method valid?
-
-  def determine
+def determine
     #  :equilateral
     #  :isosceles
     #  :scalene
-    if self.sides[0] == self.sides[1] && self.sides[1] == self.sides[2]
-      self.kind = :equilateral
+  if self.sides[0] == self.sides[1] && self.sides[1] == self.sides[2]
+    self.kind = :equilateral
 
 
-  end
+  end #end of if block 
+end #end of determine method 
 
-end
+end #end of class 
