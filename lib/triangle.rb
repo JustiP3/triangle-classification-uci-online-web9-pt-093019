5cca4f1dfaa001c136994c2da120870882f8975b
Justin Phillips <justinphil02@yahoo.com>
2019-11-11 00:14:50 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -23,7 +23,7 @@ end #end of kind method
 def valid?  #triangle inequality test and all sides > 0
   self.sides.sort
 
-  if (self.sides[0] < 1)
+  if (self.sides[0] <= 0)
     raise TriangleError
   end
   if self.sides[2] < (self.sides[1] + self.sides[0])
