0f47c5404e95a3b823c1800cafa4ee5409ee9bb4
Justin Phillips <justinphil02@yahoo.com>
2019-11-11 00:03:32 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -23,17 +23,9 @@ end #end of kind method
 def valid?  #triangle inequality test and all sides > 0
   self.sides.sort
 
-  if self.sides.detect {|side|  side <= 0 }
-    self.error_message
-  else
-   return true
-  end #end of if block
+  if (self.sides.detect {|side|  side <= 0 }) || (self.sides[2] < self.sides[1] + self.sides[0])
 
-  if self.sides[2] < self.sides[1] + self.sides[0]
-    self.error_message
-  else
-   return true
-  end #end of if block
+
 
 end #end of method valid?
 
